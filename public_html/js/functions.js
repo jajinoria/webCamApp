42373225418e39eba3b0068cbea27a86fa9e8df3
Jose <Jose@192.168.1.131>
2013-09-19 17:06:12 +0100

    first commit

---
+++ public_html/js/functions.js
@@ -0,0 +1,15 @@
+
+function snapShot() {
+   if (localMediaStream) {
+   ctx.drawImage(video, 0, 0);}
+   snapShotControler = true;
+ }
+
+function donwloadImage(){
+if (snapShotControler === false)    
+    return alert ("Take e picture first");    
+else{
+var imag = canvas.toDataURL("image/jpeg");
+imag = imag.replace("image/jpeg", "image/octet-stream");
+document.location = imag;	
+};}
---
+++ public_html/js/getUserMediaDeclaration.js
@@ -0,0 +1,12 @@
+var localMediaStream = null;
+var snapShotControler= false;
+
+var onCameraFail = function (exception) {
+        console.log('Camera did not work.', exception);
+ };
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+navigator.getUserMedia({video:true}, function (stream) {
+video.src = window.URL.createObjectURL(stream);
+localMediaStream = stream;
+}, onCameraFail);
