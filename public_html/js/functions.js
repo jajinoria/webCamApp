c9c9925a46a60e4cd473621149bbb8674841d137
Jose <Jose@192.168.1.131>
2013-09-24 12:55:56 +0100

    first step of  webRTC showing in canvasPIC, (introducing methods)

--- public_html/js/functions.js
+++ public_html/js/functions.js
@@ -1,30 +1,88 @@
 function snapShot() {
-    if (localMediaStream) {
-        ctxPIC.drawImage(canvas, 0, 0);
-    }
+    if (localStream) {
+        ctxPIC.drawImage(canvas, 0, 0);}
     snapShotControler = true;
 }
 
 function donwloadImage() {
-    if (snapShotControler === false)
-        return alert("Take e picture first");
-    else {
-        var imag = canvas.toDataURL("image/jpeg");
-        imag = imag.replace("image/jpeg", "image/octet-stream");
-        document.location = imag;
-    }
-    ;
+  if (snapShotControler === false)
+      return alert("Take e picture first");
+  else {
+     var imag = canvas.toDataURL("image/jpeg");
+     imag = imag.replace("image/jpeg", "image/octet-stream");
+     document.location = imag;};
 }
 
-function notImplementedYet()
-{
-    alert("This functionality is not implemented yet\n\
-                    Comming soon!!");
-}
 
+function notImplementedYet(){
+    alert("not implemented yet");
+    //not implemented yet
+}
 
 function drawVideoFrame() {
-    requestAnimationFrame(drawVideoFrame);
-    ctx.drawImage(video, 0, 0);
+   requestAnimationFrame(drawVideoFrame);
+   ctx.drawImage(video, 0, 0);
 };
-requestAnimationFrame(drawVideoFrame);
+   requestAnimationFrame(drawVideoFrame);
+
+
+function drawRemoteVideoFrame() {
+    requestAnimationFrame(drawRemoteVideoFrame);
+    ctxPIC.drawImage(remoteVideo, 0, 0);
+};
+requestAnimationFrame(drawRemoteVideoFrame);
+
+
+
+
+var onCameraFail = function(exception) {
+    console.log('Camera did not work.', exception);
+};
+
+function startVideoChat() {
+    var servers = null;
+
+    localPeerConnection = new webkitRTCPeerConnection(servers);
+    localPeerConnection.onicecandidate = gotLocalIceCandidate;
+
+    remotePeerConnection = new webkitRTCPeerConnection(servers);
+    remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
+    remotePeerConnection.onaddstream = gotRemoteStream;
+    localPeerConnection.addStream(localStream);
+    localPeerConnection.createOffer(gotLocalDescription);
+}
+
+function gotLocalDescription(description) {
+    localPeerConnection.setLocalDescription(description);
+    remotePeerConnection.setRemoteDescription(description);
+    remotePeerConnection.createAnswer(gotRemoteDescription);
+}
+
+function gotRemoteDescription(description) {
+    remotePeerConnection.setLocalDescription(description);
+    localPeerConnection.setRemoteDescription(description);
+}
+
+function gotRemoteStream(event) {
+    remoteVideo.src = URL.createObjectURL(event.stream);
+}
+
+function gotLocalIceCandidate(event) {
+    if (event.candidate) {
+        remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+    }
+}
+
+function gotRemoteIceCandidate(event) {
+    if (event.candidate) {
+        localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+      
+    }
+}
+
+function stopVideoChat(){    
+  localPeerConnection.close();
+  remotePeerConnection.close();
+  localPeerConnection = null;
+  remotePeerConnection = null;
+}
--- public_html/js/getUserMediaDeclaration.js
+++ public_html/js/getUserMediaDeclaration.js
@@ -1,12 +1,8 @@
-var localMediaStream = null;
-var snapShotControler = false;        
-var onCameraFail = function(exception) {
-    console.log('Camera did not work.', exception);
-};
-
+function start(){
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 window.URL = window.URL || window.webkitURL;
 navigator.getUserMedia({video: true}, function(stream) {
-    video.src = window.URL.createObjectURL(stream);
-    localMediaStream = stream;
-}, onCameraFail);
+video.src = window.URL.createObjectURL(stream);
+localStream = stream;
+}, onCameraFail);}
+
